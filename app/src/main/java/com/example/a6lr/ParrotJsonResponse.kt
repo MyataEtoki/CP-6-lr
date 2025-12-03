65cf1858789a66d2c4d62d758dae9225b1f21e12
MyataEtoki <baksyanya2006@gmail.com>
2025-12-03 04:40:39 +0300

    aaaaa

--- app/src/main/java/com/example/a6lr/ParrotJsonResponse.kt
+++ app/src/main/java/com/example/a6lr/ParrotJsonResponse.kt
@@ -4,6 +4,7 @@ package com.example.a6lr
 // [ { "name": "Какаду", "species": "Какаду", "age": 5 }, ... ]
 
 data class ParrotJsonResponse(
+    val id: Int,
     val name: String,
     val species: String,
     val age: Int
